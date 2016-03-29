82e6abce8d3d4a21beace8f0bafa38c652304683
RicciFlowing <benjamin@mathe-sellin.de>
2016-03-29 18:27:58 +0200

    added some refactoring

--- app/controllers/budgets.js
+++ app/controllers/budgets.js
@@ -1,13 +1,6 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
-  sum: Ember.computed('model.@each.amount', function(){
-    let budgets = this.get('model');
-    let amounts = budgets.mapBy('amount');
-    let sum = 0;
-    amounts.forEach(function(amount){sum+= parseInt(amount)});
-    return sum;
-  }),
   actions: {
     delete(budget){
       budget.destroyRecord();
--- app/services/income.js
+++ app/services/income.js
@@ -1,7 +1,9 @@
 import Ember from 'ember';
 
+const TOTAL = 1000;
+
 export default Ember.Service.extend({
   total(){
-    return 1000;
+    return TOTAL;
   }
 });
