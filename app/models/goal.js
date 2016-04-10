93dac1f343a96b6f7472ceedf34bba743b2b8fb0
RicciFlowing <benjamin@mathe-sellin.de>
2016-04-10 11:15:45 +0200

    Fixed a bug in the goal creation caused by undefined payments and zero amount

--- app/models/goal.js
+++ app/models/goal.js
@@ -6,12 +6,19 @@ export default DS.Model.extend({
   due: DS.attr('number'),
   amount: DS.attr('number'),
   //Geht in Budget rate: DS.attr('number'),
-  percentage: Ember.computed('amount', 'balance', function(){return Math.floor(this.get('balance')/this.get('amount')*100);}),
+  percentage: Ember.computed('amount', 'balance', function(){
+    if(Ember.isNone(this.get('amount'))){return 0;}
+    return Math.floor(this.get('balance')/this.get('amount')*100);
+  }),
   budget: DS.belongsTo('budget'),
   rate: Ember.computed.alias('budget.amount'),
   payments: Ember.computed.alias('budget.payments'),
   balance: Ember.computed('payments.@each.amount', function(){
-    let payments = this.get('payments');
+    if(Ember.isNone(this.get('payments'))){
+      Ember.Logger.log('hello');
+      return 0;
+    }
+    let payments =this.get('payments');
     let sum = 0;
     if(payments.length >0){
       let amounts = payments.mapBy('amount');
