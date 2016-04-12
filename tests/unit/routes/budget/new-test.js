d8d0f8a118596b697ec7610ecc555bcda1d0c880
RicciFlowing <benjamin@mathe-sellin.de>
2016-04-12 07:35:06 +0200

    Added a link to budgets.new on the budgets/index page

---
+++ tests/unit/routes/budget/new-test.js
@@ -0,0 +1,11 @@
+import { moduleFor, test } from 'ember-qunit';
+
+moduleFor('route:budget/new', 'Unit | Route | budget/new', {
+  // Specify the other units that are required for this test.
+  // needs: ['controller:foo']
+});
+
+test('it exists', function(assert) {
+  let route = this.subject();
+  assert.ok(route);
+});
